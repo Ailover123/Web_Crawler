87a9e8e404fb6fe26f4e3806f6782a8a241762a6
AiLover <nishalpoojary71@gmail.com>
2025-12-18 16:41:15 +0530

    Created basic config file with necessary values.

--- baseline-crawler/crawler/config.py
+++ baseline-crawler/crawler/config.py
@@ -1,1 +1,18 @@
 #For Config Values
+#Seperate file for better change management to handle the behaviour of crawler without changing the main codebase
+#Easier to test and maintain
+
+#This is where crawling starts from
+SEED_URLS = []
+
+#Maximum depth to crawl
+DEPTH_LIMIT = 0
+
+#Domains allowed to crawl, only to avoid external links
+ALLOWED_DOMAINS = []
+
+#Time to wait between requests to the same domain (in seconds)
+REQUEST_TIMEOUT = 5
+
+#User-Agent string to identify the crawler
+USER_AGENT = "BaselineCrawler/1.0"
