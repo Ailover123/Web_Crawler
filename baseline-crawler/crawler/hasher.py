64ee4de8f981e045a360c1714472e215b185ab6c
AiLover <nishalpoojary71@gmail.com>
2025-12-19 16:18:37 +0530

    Hash file got minute changes to resolve errors

--- baseline-crawler/crawler/hasher.py
+++ baseline-crawler/crawler/hasher.py
@@ -1,1 +1,13 @@
 #To hash incoming baseline data
+# Input: normalized string
+# Output: hash value
+
+import hashlib
+def hash_content(normalized_content):
+  sha = hashlib.sha256()
+  sha.update(normalized_content.encode("utf-8"))
+  # Convert string to bytes before hashing
+  return sha.hexdigest()
+  # Return hexadecimal representation of the hash
+
+  
